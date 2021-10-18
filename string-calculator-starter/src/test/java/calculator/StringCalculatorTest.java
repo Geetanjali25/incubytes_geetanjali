9bfe30d43fae15025bab865a36537804fe753bdd
Geetanjali25 <geetanjali4797@gmail.com>
2021-10-18 10:22:17 +0530

    Initial Commit

---
+++ string-calculator-starter/src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringCalculatorTest {
+
+	StringCalculator C = new StringCalculator();
+
+	@Test
+	public void test1_1(){
+		try {
+		//Empty String test case
+		assertEquals(0, C.Add(""));
+		}catch(Exception e) {
+    		//e.printStackTrace();
+    	}
+	}
+}
